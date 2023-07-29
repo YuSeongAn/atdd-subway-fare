1c9a9aabebc786e5ae7412e14093a0a3dcb72fdf
yuseongan <dhaorktm125@naver.com>
2023-07-29 13:46:05 +0900

    Append path documentation test case

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,52 @@
 package nextstep.subway.documentation;
 
 import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+
+import static nextstep.subway.acceptance.PathSteps.지하철_경로_조회;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
+    private Long sourceId = 1L;
+    private Long targetId = 1L;
+
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(sourceId, "강남역"),
+                        new StationResponse(targetId, "역삼역")
+                ), 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        var doc = document("path",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                        fieldWithPath(".distance").description("지하철 경로 전체 거리"),
+                        fieldWithPath(".stations[]").description("지하철 경로 역 목록"),
+                        fieldWithPath(".stations[].id").description("지하철 역 id"),
+                        fieldWithPath(".stations[].name").description("지하철 역 이름")
+                ));
+
+        지하철_경로_조회(spec, doc, sourceId, targetId);
     }
 }
